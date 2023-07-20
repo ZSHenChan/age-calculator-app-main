70839b723c24d82c9488144e6987685712bf97b9
Zhen <135425829+ZSHenChan@users.noreply.github.com>
2023-07-20 21:10:02 +0800

    age calculator js done

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const form = document.getElementById("form");
 
-function isValid(){
-
-    const d = new Date();
-    const tday = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
-    const day = document.getElementById("day").value;
-    const month = document.getElementById("month").value;
-    const year = document.getElementById("year").value;
-    const dateOfBirth = year+"-"+month+"-"+day;
+function isValid(dateOfBirth,tday){
 
     if (dateOfBirth>tday){
         return false;
     }
-    else if(dateOfBirth instanceof Date == false){
-        return false;
-    }
+    // else if(dateOfBirth instanceof Date == false){
+    //     return false;
+    // }
     return true;
-
 }
 
 form.addEventListener('submit',function(e){
@@ -26,7 +18,14 @@ form.addEventListener('submit',function(e){
     const errorMsgs = document.querySelectorAll('.input-box .error-msg');
     const button = document.querySelector("button");
 
-    if(!isValid()){
+    let d = new Date();
+    const tday = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
+    const day = document.getElementById("day").value;
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+    const dateOfBirth = year+"-"+month+"-"+day;
+
+    if(!isValid(dateOfBirth,tday)){
         
         redLabels.forEach(redLabel => {
             redLabel.style.color = "hsl(0, 100%, 67%)";
@@ -37,20 +36,37 @@ form.addEventListener('submit',function(e){
         errorMsgs.forEach(errorMsg => {
             errorMsg.style.display = "block";
         })
-        button.style.transform = "translate(-10px, 60px)";
+        button.style.transform = "translate(-10px, 70px)";
     }
 
     else{
-        console.log("hello");
-        redLabels.forEach(redLabel => {
-            redLabel.style.color = "black";
-        })
-        redBoxes.forEach(redbox => {
-            redbox.style.borderColor = "black";
-        })
-        errorMsgs.forEach(errorMsg => {
-            errorMsg.style.display = "none";
-        })
-        button.style.transform = "translate(-10px, 50px)";
+        // redLabels.forEach(redLabel => {
+        //     redLabel.style.color = "black";
+        // })
+        // redBoxes.forEach(redbox => {
+        //     redbox.style.borderColor = "black";
+        // })
+        // errorMsgs.forEach(errorMsg => {
+        //     errorMsg.style.display = "none";
+        // })
+        // button.style.transform = "translate(-10px, 50px)";
+
+        // calculate age
+        const newDay = document.getElementById("new-day");
+        const newMonth = document.getElementById("new-month");
+        const newYear = document.getElementById("new-year");
+
+        const ONE_DAY = 1000 * 60 * 60 * 24;
+        console.log(dateOfBirth);
+        date = new Date(dateOfBirth);
+        const diffDays = Math.round(Math.abs((d-date) / ONE_DAY));
+        const diffYear = Math.floor(diffDays/365);
+        const diffMonth = Math.floor(diffDays%365/30);
+        const diffDay = Math.floor(diffDays%365%30);
+        
+
+        newDay.innerHTML = diffDay;
+        newMonth.innerHTML = diffMonth;
+        newYear.innerHTML = diffYear;
     }
 })
--- testing.js
+++ testing.js
@@ -1,9 +1,19 @@
 const d = new Date();
 const tday = d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
-const day = "26";
-const month = "04";
-const year = "2040";
+const day = "29";
+const month = "07";
+const year = "2025";
 const date = year+"-"+month+"-"+day;
-console.log(tday);
-console.log(date);
-console.log(date>tday);
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+const sample = new Date(date);
+console.log(sample);
+const diffDays = Math.round(Math.abs((sample-d) / ONE_DAY));
+const diffYear = Math.floor(diffDays/365);
+const diffMonth = Math.floor(diffDays%365/30);
+const diffDay = Math.floor(diffDays%365%30);
+console.log(diffDays);
+
+
+
+
